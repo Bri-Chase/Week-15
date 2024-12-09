8928f26a6232299cec1980ad2c607825e8c48ccc
Bri-Chase <brianna.chase@sinclair.edu>
2024-12-08 19:21:56 -0500

    Update main-b.cpp

--- main-b.cpp
+++ main-b.cpp
@@ -10,12 +10,12 @@
 // Templated function for half
 template <typename T>
 T half(T value) {
-    if constexpr (is_floating_point<T>value) {
+    if constexpr (std::is_floating_point<T>::value) {
         return value / 2.0;
-    } else if constexpr (is_integral<T>value) {
-        return static_cast<T>(round(value / 2.0));
+    } else if constexpr (std::is_integral<T>::value) {
+        return static_cast<T>(std::round(value / 2.0));
     } else {
-        static_assert(is_arithmetic<T>value, "Unsupported type for half function");
+        static_assert(std::is_arithmetic<T>::value, "Unsupported type for half function");
     }
 }
 
@@ -23,15 +23,16 @@ T half(T value) {
 int main() {
     // Test with integers
     int intVal = 5;
-    cout << "Half of " << intVal << " is " << half(intVal) << endl;
+    std::cout << "Half of " << intVal << " is " << half(intVal) << std::endl;
 
     // Test with float
     float floatVal = 5.5f;
-    cout << "Half of " << floatVal << " is " << half(floatVal) << endl;
+    std::cout << "Half of " << floatVal << " is " << half(floatVal) << std::endl;
 
     // Test with double
     double doubleVal = 10.25;
-    cout << "Half of " << doubleVal << " is " << half(doubleVal) << endl;
+    std::cout << "Half of " << doubleVal << " is " << half(doubleVal) << std::endl;
 
     return 0;
 }
+
